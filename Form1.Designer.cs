efb62edf8c5b697e7d4c883a64d39daf017aeca2
Joel Blake <blakejoel05@gmail.com>
2024-03-06 12:02:49 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Othello
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Play = new System.Windows.Forms.Button();
+            this.Rules = new System.Windows.Forms.Button();
+            this.Settings = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Play.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(103)))), ((int)(((byte)(54)))));
+            this.Play.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Play.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Play.Location = new System.Drawing.Point(260, 224);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(200, 100);
+            this.Play.TabIndex = 0;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Rules
+            // 
+            this.Rules.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Rules.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(103)))), ((int)(((byte)(54)))));
+            this.Rules.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Rules.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Rules.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Rules.Location = new System.Drawing.Point(260, 330);
+            this.Rules.Name = "Rules";
+            this.Rules.Size = new System.Drawing.Size(200, 100);
+            this.Rules.TabIndex = 1;
+            this.Rules.Text = "Rules";
+            this.Rules.UseVisualStyleBackColor = false;
+            this.Rules.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Settings
+            // 
+            this.Settings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(103)))), ((int)(((byte)(54)))));
+            this.Settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Settings.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Settings.Location = new System.Drawing.Point(260, 436);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(200, 100);
+            this.Settings.TabIndex = 2;
+            this.Settings.Text = "Settings";
+            this.Settings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.Settings.UseVisualStyleBackColor = false;
+            this.Settings.Click += new System.EventHandler(this.Settings_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(103)))), ((int)(((byte)(54)))));
+            this.Quit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Quit.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Quit.Location = new System.Drawing.Point(260, 542);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(200, 100);
+            this.Quit.TabIndex = 3;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = false;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(103)))), ((int)(((byte)(54)))));
+            this.label1.Font = new System.Drawing.Font("Broadway", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(62, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(584, 151);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Othello";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(45)))), ((int)(((byte)(87)))));
+            this.ClientSize = new System.Drawing.Size(704, 681);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.Rules);
+            this.Controls.Add(this.Play);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Play;
+        private Button Rules;
+        private Button Settings;
+        private Button Quit;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace Othello
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
